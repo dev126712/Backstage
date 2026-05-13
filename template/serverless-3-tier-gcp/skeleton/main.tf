52c8a38c017aa3c9484ea5ec4b4e7e9ae802e3d7
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2026-05-13 17:01:28 -0400

    Update main.tf

--- template/serverless-3-tier-gcp/skeleton/main.tf
+++ template/serverless-3-tier-gcp/skeleton/main.tf
@@ -4,8 +4,8 @@ module "cloud_run" {
   vpc    = "serverless-app-vpc"
   subnet = "serverless-app-subnet"
   # cloud run configuration
-  cloud_run_region              = "us-central1"
-  project_id                    = "project-test-490416"
+  cloud_run_region              = "${{ values.cloud_run_region }}"
+  project_id                    ="${{ values.project_id }}"
   google_cloud_run_service_name = "test-service"
 
   backend_container_image  = "us-central1-docker.pkg.dev/project-test-490416/app-images/backend:latest3"
