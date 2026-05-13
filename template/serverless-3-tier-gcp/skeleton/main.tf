6ca049d25eaaa49133e81936b438c9bae43fa1e3
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2026-05-13 16:36:43 -0400

    Create main.tf

---
+++ template/serverless-3-tier-gcp/skeleton/main.tf
@@ -0,0 +1,17 @@
+module "cloud_run" {
+  source = "git::https://github.com/dev126712/terraform-modules.git//serverless-3-Tier?ref=main"
+
+  vpc    = "serverless-app-vpc"
+  subnet = "serverless-app-subnet"
+  # cloud run configuration
+  cloud_run_region              = "us-central1"
+  project_id                    = "project-test-490416"
+  google_cloud_run_service_name = "test-service"
+
+  backend_container_image  = "us-central1-docker.pkg.dev/project-test-490416/app-images/backend:latest3"
+  frontend_container_image = "us-central1-docker.pkg.dev/project-test-490416/app-images/frontend:latest3"
+
+  database_name = "app-db-instance"
+  database_version = "POSTGRES_15"
+  database_tier = "db-f1-micro" 
+}
